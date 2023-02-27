1bcecf3e31613179adee3c063fdb833518deeee0
Swapnil <swapnilbendal155@gmail.com>
2023-02-27 20:10:21 +0530

    implement stripe

--- src/components/Checkout/CheckoutModel.jsx
+++ src/components/Checkout/CheckoutModel.jsx
@@ -1,7 +1,47 @@
-import { Fragment } from "react";
-import { Form, Modal } from "react-bootstrap";
+import {
+  CardCvcElement,
+  CardExpiryElement,
+  CardNumberElement,
+  Elements,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { Fragment, useState } from "react";
+import {
+  Button,
+  Col,
+  Form,
+  FormGroup,
+  FormLabel,
+  FormText,
+  Modal,
+  Row,
+  Spinner,
+} from "react-bootstrap";
 
-const CheckoutModel = (props) => {
+const CARD_ELEMENT_OPTIONS = {
+  style: {
+    base: {
+      fontSize: "16px",
+      color: "#424770",
+      "::placeholder": {
+        color: "#aab7c4",
+      },
+    },
+    invalid: {
+      color: "#9e2146",
+    },
+  },
+};
+
+const stripePromise = loadStripe(
+  "pk_test_51MZrheSDSUOhX3MpsPyM1G4z4IJq5LIjpL8SYBg2FpEB9nXoQLzJh7fELcaQffdZqlbdUqMK56nnmKWen9mfhiWM00Gt1RnFeu"
+);
+
+const MakePayment = (props) => {
+
+  
+  const [loader, setLoader] = useState(false);
+
   return (
     <Fragment>
       <Modal
@@ -29,12 +69,70 @@ const CheckoutModel = (props) => {
           </div>
         </Modal.Header>
         <Modal.Body>
-            <Form>
-                
-            </Form>
+          <Form>
+            <Row>
+              <Col sm={12}>
+                <FormLabel> Card Number</FormLabel>
+                <div className="border rounded p-2 w-auto">
+                  <CardNumberElement options={CARD_ELEMENT_OPTIONS} />{" "}
+                </div>
+              </Col>
+
+              <Col sm={6}>
+                <FormGroup className="mb-3" controlId="formBasiProfile">
+                  <FormLabel> Expiry</FormLabel>
+                  <div className="border rounded p-2 w-auto">
+                    <CardExpiryElement options={CARD_ELEMENT_OPTIONS} />{" "}
+                  </div>
+                </FormGroup>
+              </Col>
+
+              <Col sm={6}>
+                <FormGroup className="mb-3" controlId="formBasiProfile">
+                  <FormLabel> CVV</FormLabel>
+                  <div className="border rounded p-2 w-auto">
+                    <CardCvcElement options={CARD_ELEMENT_OPTIONS} />{" "}
+                  </div>
+                </FormGroup>
+              </Col>
+            </Row>
+
+            <FormGroup className="text-center mt-3">
+              <Button variant="primary" type="submit" disabled={loader}>
+                {!loader ? (
+                  `Buy $ ${props.prices}`
+                ) : (
+                  <Fragment>
+                    <Spinner
+                      size="sm"
+                      className="me-3"
+                      animation="border"
+                      variant="light"
+                    />
+                    {`Buy $ ${props.prices}`}
+                  </Fragment>
+                )}
+              </Button>
+            </FormGroup>
+          </Form>
         </Modal.Body>
       </Modal>
     </Fragment>
   );
 };
+
+const CheckoutModel = (props) => {
+  return (
+    <Elements stripe={stripePromise}>
+      <MakePayment
+        show={props.show}
+        onHide={props.onHide}
+        cars={props.cars}
+        prices={props.prices}
+        subcription={props.subcription}
+      />
+    </Elements>
+  );
+};
+
 export default CheckoutModel;
